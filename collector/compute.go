0becb41ccda07d9b1e7f2a44edab3e266086c94c
DazWilkin <daz.wilkin@gmail.com>
2019-12-20 14:21:06 -0800

    Added Compute Engine ForwardingRules

--- collector/compute.go
+++ collector/compute.go
@@ -17,24 +17,33 @@ type ComputeCollector struct {
 	client   *http.Client
 	projects []*cloudresourcemanager.Project
 
-	Count *prometheus.Desc
+	Instances       *prometheus.Desc
+	ForwardingRules *prometheus.Desc
 }
 
 // NewComputeCollector returns a new ComputeCollector
 func NewComputeCollector(client *http.Client, projects []*cloudresourcemanager.Project) *ComputeCollector {
 	fqName := name("compute_engine")
-	labelKeys := []string{
-		"project",
-		"region",
-	}
 	return &ComputeCollector{
 		client:   client,
 		projects: projects,
 
-		Count: prometheus.NewDesc(
+		Instances: prometheus.NewDesc(
 			fqName("instances"),
 			"Number of instances",
-			labelKeys,
+			[]string{
+				"project",
+				"zone",
+			},
+			nil,
+		),
+		ForwardingRules: prometheus.NewDesc(
+			fqName("forwardingrules"),
+			"Number of forwardingrules",
+			[]string{
+				"project",
+				"region",
+			},
 			nil,
 		),
 	}
@@ -57,50 +66,92 @@ func (c *ComputeCollector) Collect(ch chan<- prometheus.Metric) {
 			defer wg.Done()
 			log.Printf("[ComputeCollector] Project: %s", p.ProjectId)
 
-			// Compute Engine API instances.get requires zone
-			// Must repeat the call for all possible zones
-			zoneList, err := computeService.Zones.List(p.ProjectId).Context(ctx).Do()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			for _, z := range zoneList.Items {
-				wg.Add(1)
-				go func(z *compute.Zone) {
-					defer wg.Done()
-					req := computeService.Instances.List(p.ProjectId, z.Name).MaxResults(500)
-					count := 0
-					// Page through more results
-					if err := req.Pages(ctx, func(page *compute.InstanceList) error {
-						count += len(page.Items)
-						// for _, instance := range page.Items {
-						// 	instance.
-						// }
-						return nil
-					}); err != nil {
-						log.Println(err)
-						return
-					}
-					if count != 0 {
-						ch <- prometheus.MustNewConstMetric(
-							c.Count,
-							prometheus.GaugeValue,
-							float64(count),
-							[]string{
-								p.ProjectId,
-								z.Name,
-							}...,
-						)
-					}
-				}(z)
-			}
-		}(p)
+			wg.Add(1)
+			go func(p *cloudresourcemanager.Project) {
+				defer wg.Done()
+				// Compute Engine API instances.list requires zone
+				// Must repeat the call for all possible zones
+				zoneList, err := computeService.Zones.List(p.ProjectId).Context(ctx).Do()
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				for _, z := range zoneList.Items {
+					wg.Add(1)
+					go func(z *compute.Zone) {
+						defer wg.Done()
+						req := computeService.Instances.List(p.ProjectId, z.Name).MaxResults(500)
+						count := 0
+						// Page through more results
+						if err := req.Pages(ctx, func(page *compute.InstanceList) error {
+							count += len(page.Items)
+							// for _, instance := range page.Items {
+							// 	instance.
+							// }
+							return nil
+						}); err != nil {
+							log.Println(err)
+							return
+						}
+						if count != 0 {
+							ch <- prometheus.MustNewConstMetric(
+								c.Instances,
+								prometheus.GaugeValue,
+								float64(count),
+								[]string{
+									p.ProjectId,
+									z.Name,
+								}...,
+							)
+						}
+					}(z)
+				}
+			}(p)
 
+			wg.Add(1)
+			go func(p *cloudresourcemanager.Project) {
+				defer wg.Done()
+				// Compute Engine API forwardingrules.list requires region
+				// Must repeat call for all possible regions
+				regionList, err := computeService.Regions.List(p.ProjectId).Context(ctx).Do()
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				for _, r := range regionList.Items {
+					wg.Add(1)
+					go func(r *compute.Region) {
+						defer wg.Done()
+						req := computeService.ForwardingRules.List(p.ProjectId, r.Name).MaxResults(500)
+						count := 0
+						if err := req.Pages(ctx, func(page *compute.ForwardingRuleList) error {
+							count += len(page.Items)
+							return nil
+						}); err != nil {
+							log.Println(err)
+							return
+						}
+						if count != 0 {
+							ch <- prometheus.MustNewConstMetric(
+								c.ForwardingRules,
+								prometheus.GaugeValue,
+								float64(count),
+								[]string{
+									p.ProjectId,
+									r.Name,
+								}...,
+							)
+						}
+					}(r)
+				}
+			}(p)
+		}(p)
 	}
 	wg.Wait()
 }
 
 // Describe implements Prometheus' Collector interface and is used to describe metrics
 func (c *ComputeCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.Count
+	ch <- c.Instances
+	ch <- c.ForwardingRules
 }
