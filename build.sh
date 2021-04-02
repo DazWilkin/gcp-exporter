18c318bce98c348966bbc8ada2ceefcfd2272a7f
DazWilkin <daz.wilkin@gmail.com>
2021-04-02 13:02:46 -0700

    r/dockerhub/ghcr

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REPO="dazwilkin/gcp-exporter" VERSION=$(uname --kernel-release) COMMIT=$(git rev-parse HEAD) && \
+export REPO="ghcr.io/dazwilkin/gcp-exporter" VERSION=$(uname --kernel-release) COMMIT=$(git rev-parse HEAD) && \
 docker build --rm --file=Dockerfile --build-arg=VERSION="${VERSION}" --build-arg=COMMIT="${COMMIT}" --tag=${REPO}:${COMMIT} . && \
 docker push ${REPO}:${COMMIT} && \
 sed --in-place "s|${REPO}:[0-9a-f]\{40\}|${REPO}:${COMMIT}|g" ./docker-compose.yml &&
