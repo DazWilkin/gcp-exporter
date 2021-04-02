2c60a0665c71302a3ed058b069eee2d39d9361ca
DazWilkin <daz.wilkin@gmail.com>
2021-04-02 13:06:32 -0700

    Tweaked

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export REPO="ghcr.io/dazwilkin/gcp-exporter" VERSION=$(uname --kernel-release) COMMIT=$(git rev-parse HEAD) && \
-docker build --rm --file=Dockerfile --build-arg=VERSION="${VERSION}" --build-arg=COMMIT="${COMMIT}" --tag=${REPO}:${COMMIT} . && \
-docker push ${REPO}:${COMMIT} && \
-sed --in-place "s|${REPO}:[0-9a-f]\{40\}|${REPO}:${COMMIT}|g" ./docker-compose.yml &&
+REPO="ghcr.io/dazwilkin/gcp-exporter"
+VERSION=$(uname --kernel-release)
+COMMIT=$(git rev-parse HEAD)
+
+docker build \
+--rm \
+--file=Dockerfile \
+--build-arg=VERSION="${VERSION}" \
+--build-arg=COMMIT="${COMMIT}" \
+--tag=${REPO}:${COMMIT} \
+.
+
+docker push ${REPO}:${COMMIT}
+
+sed --in-place "s|${REPO}:[0-9a-f]\{40\}|${REPO}:${COMMIT}|g" ./docker-compose.yml
 sed --in-place "s|${REPO}:[0-9a-f]\{40\}|${REPO}:${COMMIT}|g" ./README.md
